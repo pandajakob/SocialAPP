d2d979f081af82808cf39840b0dfe447ca8c2be0
pandajakob <jakobm.dev@gmail.com>
2026-09-19 18:34:52 +0200

    update paths

--- backend/src/main/java/socialapp/backend/users/DTO/StandardUserResponseDTO.java
+++
@@ -1,9 +0,0 @@
-package socialapp.backend.users.DTO;
-
-import socialapp.backend.categories.Category;
-
-import java.util.List;
-import java.util.UUID;
-
-public record StandardUserResponseDTO(UUID id, String firstName, String lastName, String email, Integer age,
-                                      List<Category> interests, String phoneNumber) { }
--- backend/src/test/java/socialapp/backend/categories/CategoryControllerTest.java
+++
@@ -1,7 +0,0 @@
-package socialapp.backend.categories;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class CategoryControllerTest {
-
-}

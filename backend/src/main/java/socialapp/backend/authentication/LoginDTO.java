a4dde8585353cac236fad5d3a67dac8540d8245f
pandajakob <jakobm.dev@gmail.com>
2026-09-19 18:46:17 +0200

    (fix) update paths

--- backend/src/main/java/socialapp/backend/authentication/LoginDTO.java
+++
@@ -1,7 +0,0 @@
-package socialapp.backend.authentication;
-
-import socialapp.backend.shared.domain_primitives.Email;
-import socialapp.backend.shared.domain_primitives.Password;
-
-public record LoginDTO(String email, String password)
-{ }
--- backend/src/main/java/socialapp/backend/posts/exceptions/PostNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package socialapp.backend.posts.exceptions;
-
-import java.util.UUID;
-
-public class PostNotFoundException extends RuntimeException {
-    public PostNotFoundException(UUID id) {
-        super("Post with id: " + id.toString() + " could not found");
-    }
-}
